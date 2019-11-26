679509b03d49fd76926a440144d475787220033e
unknown <michael.j.lukiman@gmail.com>
2019-11-25 20:06:37 -0500

    add new class

---
+++ Arrable.py
@@ -0,0 +1,18 @@
+
+from BTreeImplementation import BTreeImplementation
+
+class Arrable:
+    
+    def __init__(self, file=None):
+        indices = BTreeImplementation
+        records = []
+        
+        if data_path:
+            with open(data_path, "r") as f:
+                for line in f:
+                    key, val = line.split("|")
+                    if key[0].isdigit():
+                        truth[int(key)] = int(val)
+                        db.insert(int(key), int(val))
+        
+    
