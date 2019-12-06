e7c6baee5293cfa910a66814533bb459817fb2a2
bzmull <bmuller94@gmail.com>
2019-12-05 20:38:36 -0500

    parser function

--- run_command_file.py
+++ run_command_file.py
@@ -21,5 +21,26 @@ def time_command(interpreted_command: str, obj_dict, trials=1, print_out=True):
     exec_string = f"'q.'+{interpreted_command}, globals(), {obj_dict}" # Literally execute the strings
     duration = timeit.timeit(lambda: exec(exec_string))
     if print_out: print(duration)
+
+def remove_comments(line, sep):
+    for s in sep:
+        i = line.find(s)
+        if i >= 0:
+            line = line[:i]
+    return line.strip()
+
+def parse(input_path: str):
+    with open(input_path) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+
+    parsed = []
+    for line in content:
+        new_line = remove_comments(line, "//")
+        if new_line != '':
+            split_line = new_line.split(" := ", 1)
+            parsed.append(split_line)
+            
+    return parsed
     
             
